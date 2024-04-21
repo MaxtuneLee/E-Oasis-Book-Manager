d26e377ad83d3325d5ede07ebcc3947641bbce94
MaxtuneLee <max@xox.im>
2024-04-21 19:37:02 +0800

    feat: new shelf form

--- src/components/ShelfSidebar/index.tsx
+++ src/components/ShelfSidebar/index.tsx
@@ -1,16 +1,43 @@
 "use client"
-import { AlignLeft, Book, Binary, PenTool, Smile } from "lucide-react"
+import { AlignLeft, Book, Binary, PenTool, Smile, Plus } from "lucide-react"
 import FullButton from "../FullButton"
 import { Button } from "../ui/button"
 import { SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription, Sheet } from "../ui/sheet"
 import { usePathname } from "next/navigation"
 import { useCallback } from "react"
 import Link from "next/link"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { DialogClose } from "@radix-ui/react-dialog"
 
 // million-ignore
 const ShelfSidebar = () => {
     const path = usePathname()
     const type = path.split('/')[2]
+
+    const addNewShelfSchema = z.object({
+        shelfName: z.string().min(1),
+        id: z.string().regex(/^[A-Za-z]/).min(1)
+    })
+
+    const addNewShelfSchemaForm = useForm<z.infer<typeof addNewShelfSchema>>({
+        resolver: zodResolver(addNewShelfSchema),
+        defaultValues: {
+            shelfName: '',
+            id: ''
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof addNewShelfSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
 
     const calButtonType = useCallback((currentType: string) => {
         if (currentType === type) {
@@ -56,8 +83,60 @@ const ShelfSidebar = () => {
                     <div className=" w-full">
                         <Button variant={calButtonType('light-novel')} asChild><Link href='./light-novel'><Smile className="mr-2 h-4 w-4" />轻小说</Link></Button>
                     </div>
+                    <div className=" w-full">
+                        <Dialog>
+                            <DialogTrigger>
+                                <Button variant="outline" ><Plus className="mr-2 h-4 w-4" />添加书架</Button>
+                            </DialogTrigger>
+                            <Form {...addNewShelfSchemaForm}>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>添加新书架</DialogTitle>
+                                        <form onSubmit={addNewShelfSchemaForm.handleSubmit(onSubmit)} className="space-y-8 pt-8">
+                                            <FormField
+                                                control={addNewShelfSchemaForm.control}
+                                                name="shelfName"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>书架名称</FormLabel>
+                                                        <FormControl>
+                                                            <Input placeholder="your shelf name" {...field} />
+                                                        </FormControl>
+                                                        <FormDescription>
+                                                            这是页面显示的书架名称
+                                                        </FormDescription>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )} />
+                                            <FormField
+                                                control={addNewShelfSchemaForm.control}
+                                                name="id"
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel>书架 ID</FormLabel>
+                                                        <FormControl>
+                                                            <Input placeholder="your shelf id" {...field} />
+                                                        </FormControl>
+                                                        <FormDescription>
+                                                            这是书架的唯一标识符
+                                                        </FormDescription>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )} />
+                                            <DialogFooter>
+                                                <DialogClose asChild>
+                                                    <Button variant="outline">取消</Button>
+                                                </DialogClose>
+                                                <Button type="submit">确认</Button>
+                                            </DialogFooter>
+                                        </form>
+                                    </DialogHeader>
+                                </DialogContent>
+                            </Form>
+                        </Dialog>
+                    </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
